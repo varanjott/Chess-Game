8c84ca76b46c8f4a7cb5bc13e765f10d2d8fd00c
varanjott <jashanjeet.singh@axelerant.com>
2026-05-20 21:32:41 +0530

    Configure base path and homepage for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chess-game/",
+  base: "/Chess-Game/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
